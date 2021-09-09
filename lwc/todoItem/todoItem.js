9148e015b91b75d878458a2e7569c2068420df0e
Temqtik <78302199+Temqtik@users.noreply.github.com>
2021-09-09 09:50:38 +0300

    update controller and list

--- lwc/todoItem/todoItem.js
+++ lwc/todoItem/todoItem.js
@@ -4,12 +4,32 @@ export default class TodoItem extends LightningElement {
 
     @api
     todo;
-
+    //chevrondown
+    //utility:chevronright
     @api
     allSubtodos;
 
+    listIsHidden = true;
+    listIcon = 'utility:chevronright';
+
+
+    get listIsEmpty(){
+        return this.allSubtodos.filter(x => x.Todo__c === this.todo.Id).length?false:true;
+    }
+
     get subtodos(){
-        return this.allSubtodos.filter(x => x.Todo__c === this.todo.Id);
+        let result = this.allSubtodos.filter(x => x.Todo__c === this.todo.Id);
+        return result;
+    }
+
+    viewHideSubtodos(){
+        if(!this.listIsHidden) {
+            this.listIsHidden = true;
+            this.listIcon = 'utility:chevronright';
+        } else {
+            this.listIsHidden = false;
+            this.listIcon = 'utility:chevrondown';
+        }
     }
 
 }
